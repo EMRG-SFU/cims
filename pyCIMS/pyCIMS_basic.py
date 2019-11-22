66fe108b45f04b241240fd49b91f5dfd9a845608
Jillian Anderson <12687428+jillianderson8@users.noreply.github.com>
2019-11-22 14:03:22 -0800

    Documented basic pyCIMS code
    Former-commit-id: 6635b88c4c59ee3c41d10c825744e32d0f0ccb24

--- pyCIMS/pyCIMS_basic.py
+++ pyCIMS/pyCIMS_basic.py
@@ -4,8 +4,21 @@ import pyCIMS
 
 file = 'pycims_prototype/pyCIMS_model_description.xlsm'
 
+# Validate the Model Description
 mv = pyCIMS.ModelValidator(file)
 mv.validate(verbose=True)
+
+# Create a model description reader
 pycims_reader = pyCIMS.Reader(infile=file,
                               sheet_map={'model': 'Model', 'incompatible': 'Incompatible'})
+
+# From the reader, create a model
 my_model = pyCIMS.Model(pycims_reader)
+
+# Access the networkx graph of the model
+print(my_model.graph)
+len(my_model.graph.nodes)
+len(my_model.graph.edges)
+
+# Search for a node within the graph
+print(my_model.search_nodes("Alberta"))
