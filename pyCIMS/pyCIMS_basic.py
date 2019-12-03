5057fb85c777fa2e2ce8895507251f686ee5225b
Jillian Anderson <12687428+jillianderson8@users.noreply.github.com>
2019-12-03 10:41:31 -0800

    Updating Code to work with new Model Description
    Former-commit-id: c139f93bffbba2a12c97e2cf4ea602b1952a1794

--- pyCIMS/pyCIMS_basic.py
+++ pyCIMS/pyCIMS_basic.py
@@ -6,9 +6,10 @@ file = 'pycims_prototype/pyCIMS_model_description.xlsm'
 
 # Create a model description reader
 my_reader = pyCIMS.Reader(infile=file,
-                          sheet_map={'model': 'Model', 'incompatible': 'Incompatible'},
-                          node_col='Node',
-                          extra_cols=['Demand?'])
+                          sheet_map={'model': 'Model',
+                                     'incompatible': 'Incompatible',
+                                     'default_tech': 'Technologies'},
+                          node_col='Node')
 # If you want to validate that the model is defined correctly
 model_warnings = my_reader.validate_model(verbose=False)
 pprint(model_warnings)
