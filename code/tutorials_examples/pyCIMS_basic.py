2f82c10a6ec943b0437d69650592741870855d0e
Jillian Anderson <12687428+jillianderson8@users.noreply.github.com>
2019-12-19 09:05:26 -0800

    Move tutorials to a specific folder
    Former-commit-id: ce346c4f9be5ccdb3b267f6436bce3b3c39b0ae0

--- code/tutorials_examples/pyCIMS_basic.py
+++ code/tutorials_examples/pyCIMS_basic.py
@@ -4,6 +4,10 @@ from pprint import pprint
 
 file = 'pycims_prototype/pyCIMS_model_description.xlsm'
 
+my_validator = pyCIMS.ModelValidator(file)
+my_validator.validate(raise_warnings=False)
+my_validator.warnings
+
 # Create a model description reader
 my_reader = pyCIMS.Reader(infile=file,
                           sheet_map={'model': 'Model',
@@ -11,8 +15,8 @@ my_reader = pyCIMS.Reader(infile=file,
                                      'default_tech': 'Technologies'},
                           node_col='Node')
 # If you want to validate that the model is defined correctly
-model_warnings = my_reader.validate_model(verbose=False)
-pprint(model_warnings)
+# model_warnings = my_reader.validate_model(verbose=False)
+# pprint(model_warnings)
 
 # Create a model from the reader
 my_model = pyCIMS.Model(my_reader)
