12edb89b7a9661063d181b9e6bd658d7400d8efe
Maude Lachaine <maudelachaine@Semaphores-MacBook.local>
2019-12-20 16:08:42 -0500

    gets through all demand nodes
    Former-commit-id: f24db24b759bb615a388ff1e7180a342be7de045

---
+++ pyCIMS/pyCIMS_basicMaude.py
@@ -0,0 +1,31 @@
+# Run this from the the /pycims_prototype/ directory
+import os
+os.chdir(os.path.join(os.getcwd(), 'pyCIMS'))
+
+
+import pyCIMS
+from pprint import pprint
+
+
+
+file = '../pyCIMS_model_description.xlsm'
+
+
+# Create a model description reader
+my_reader = pyCIMS.Reader(infile=file,
+                          sheet_map={'model': 'Model',
+                                     'incompatible': 'Incompatible',
+                                     'default_tech': 'Technologies'},
+                          node_col='Node')
+# If you want to validate that the model is defined correctly
+model_warnings = my_reader.validate_model(verbose=False)
+pprint(model_warnings)
+
+# Create a model from the reader
+my_model = pyCIMS.Model(my_reader)
+
+# Build the model graph
+my_model.build_graph()
+
+# Run the Model
+my_model.run()
