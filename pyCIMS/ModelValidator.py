3e4cf2c3f28b6ee9310805c2ccb60a6fdc013c76
Jillian Anderson <12687428+jillianderson8@users.noreply.github.com>
2020-06-18 09:13:15 -0700

    identifying nodes which request services of themselves
    Former-commit-id: bf85ab7574e969862b17263d46562f9bbfab0bd0

--- pyCIMS/ModelValidator.py
+++ pyCIMS/ModelValidator.py
@@ -23,14 +23,29 @@ class ModelValidator:
         mdf = model_df.loc[1:, all_cols]  # Create df, drop irrelevant columns & skip first, empty row
 
         self.model_df = mdf
-        self.index2node_map = self.model_df[self.node_col].ffill() 
+        self.index2node_map = self.model_df[self.node_col].ffill()
+        self.index2node_index_map = self._create_index_to_node_index_map()
 
     def find_roots(self):
-        root_idx = self.model_df[(self.model_df['Parameter'] == 'Competition type') & 
+        root_idx = self.model_df[(self.model_df['Parameter'] == 'Competition type') &
                                  (self.model_df['Value'] == 'Root')].index
         root_nodes = set([self.index2node_map[ri] for ri in root_idx])
 
-        return root_nodes 
+        return root_nodes
+
+    def _create_index_to_node_index_map(self):
+        # Record the index for every node name
+        node_indexes = []
+        for i, name in zip(self.model_df.index, self.model_df[self.node_col]):
+            if name:
+                node_indexes.append(i)
+            else:
+                node_indexes.append(None)
+
+        # Forward fill to get map
+        index_to_node_index_map = pd.Series(node_indexes, index=self.model_df.index).ffill()
+
+        return index_to_node_index_map
 
     def validate(self, verbose=True, raise_warnings=False):
         def invalid_competition_type():
@@ -40,13 +55,13 @@ class ModelValidator:
                                'Sector No Tech',
                                'Tech Compete',
                                'Fixed Ratio']
-            
+
             invalid_nodes = []
             comp_types = self.model_df[self.model_df['Parameter'] == 'Competition type']
             for index, value in zip(comp_types.index, comp_types['Value']):
                 if value not in valid_comp_type:
                     invalid_nodes.append((index, self.index2node_map[index]))
-                
+
             if len(invalid_nodes) > 0:
                 self.warnings['invalid_competition_type'] = invalid_nodes
 
@@ -61,7 +76,7 @@ class ModelValidator:
                 w = "{} nodes had invalid competition types. {}".format(len(invalid_nodes),
                                                                                   more_info if len(invalid_nodes) else "")
                 warnings.warn(w)
-        
+
         def unspecified_nodes(p, r):
             referenced_unspecified = [(i, v) for i, v in r.iteritems() if v not in p.values]
 
@@ -107,7 +122,7 @@ class ModelValidator:
                 cand = node_name_indexes.loc[:i]
                 node_name_index = cand.index.max()
                 node_name = list(cand)[-1]
-                if x is None: 
+                if x is None:
                     mismatched.append((node_name_index, node_name, None))
                 else:
                     branch_node_name = x.split('.')[-1]
@@ -131,7 +146,7 @@ class ModelValidator:
         def nodes_no_provided_service(p):
             nodes = self.model_df[self.node_col].dropna()
             nodes_that_provide = [self.index2node_map[i] for i, v in p.iteritems()]
-            nodes_no_service = [(i,n) for i, n in nodes.iteritems() if n not in nodes_that_provide] 
+            nodes_no_service = [(i,n) for i, n in nodes.iteritems() if n not in nodes_that_provide]
 
             if len(nodes_no_service) > 0:
                 self.warnings['nodes_no_provided_service'] = nodes_no_service
@@ -148,6 +163,70 @@ class ModelValidator:
                                                                                      more_info if len(nodes_no_service) else "")
                 warnings.warn(w)
 
+        def nodes_requesting_self(p, r):
+            """
+            Identifies any nodes which request services of themselves. Adds (index, Node Name) pairs
+            to the warnings dictionary, under the key "nodes_requesting_self".
+
+            If `verbose` is `True` will print nodes which were identified . If `raise_warnings` is
+            `True` will raise warnings to indicate which nodes were identified.
+
+            Parameters
+            ----------
+            p : pd.Series
+                A series of branch names for any line in the model description where "Service
+                provided" is the parameter. Index of this series is the line number of the row where
+                it appears in the model description.
+
+            r : pd.Series
+                A series of branch names for any line in the model description where "Service
+                requested" is the parameter. Index of this series is the line number of the row
+                where is appears in the model description.
+
+            Returns
+            -------
+            None
+            """
+            # Use Index to Node Index map to determine node index for all service provided lines
+            p_df = pd.DataFrame(index=p.index)
+            p_df['branch'] = p
+            p_df['node_index'] = [self.index2node_index_map[i] for i, branch in p.iteritems()]
+
+            # Use Index to Node Index map to determine node index for all service requested lines
+            r_df = pd.DataFrame(index=r.index)
+            r_df['branch'] = r
+            r_df['node_index'] = [self.index2node_index_map[i] for i, branch in r.iteritems()]
+
+            # Merge
+            merge_df = r_df.reset_index().merge(p_df.reset_index(),
+                                                on='node_index',
+                                                suffixes=('_requested', '_supplied'))
+
+            # Filter where request and provide are equal
+            conflicts = merge_df[merge_df['branch_requested'] == merge_df['branch_supplied']]
+
+            # Get Index, Branch pairs for logging
+            self_requesting = [(i, b) for i, b in zip(conflicts['index_requested'],
+                                                      conflicts['branch_requested'])]
+
+            # Add violating nodes to warnings dictionary
+            if len(self_requesting) > 0:
+                self.warnings['nodes_requesting_self'] = self_requesting
+
+            # Print Problems
+            if verbose:
+                more_info = "See ModelValidator.warnings['nodes_requesting_self'] for more info"
+                print("{} nodes requested services of themselves. "
+                      "{}".format(len(self_requesting),
+                                  more_info if len(self_requesting) else ""))
+            # Raise Warnings
+            if raise_warnings:
+                more_info = "See ModelValidator.warnings['nodes_requesting_self'] for more info"
+                w = "{} nodes requested services of themselves " \
+                    "{}".format(len(nodes_requesting_self),
+                                more_info if len(nodes_requesting_self) else "")
+                warnings.warn(w)
+
         providers = self.model_df[self.model_df['Parameter'] == 'Service provided']['Branch']
         requested = self.model_df[self.model_df['Parameter'] == 'Service requested']['Branch']
         roots = self.find_roots()
@@ -157,3 +236,4 @@ class ModelValidator:
         unreferenced_nodes(providers, requested, roots)
         nodes_no_provided_service(providers)
         invalid_competition_type()
+        nodes_requesting_self(providers, requested)
