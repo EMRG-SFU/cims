3ebbf2329819cd7eca99898977b63706569044ac
Jillian Anderson <12687428+jillianderson8@users.noreply.github.com>
2021-05-26 11:34:52 -0700

    Starting restructuring of stock allocation code
    Former-commit-id: 3dd0802788237122dced820b3eb4eea244648019

--- pyCIMS/stock_allocation.py
+++ pyCIMS/stock_allocation.py
@@ -1,6 +1,216 @@
 from . import utils
+from .model import ProvidedQuantity
+
+import warnings
+
+#############################
+# Stock Allocation
+#############################
+def get_existing_stock_per_tech(model, sub_graph, node, year):
+    node_year_data = model.graph.nodes[node][year]
+
+    existing_stock_per_tech = {}
+    for tech in node_year_data['technologies']:
+        tech_existing = model.calc_existing_stock(sub_graph, node, year, tech)
+        existing_stock_per_tech[tech] = tech_existing
+
+    return existing_stock_per_tech
 
 
+def calc_new_stock_demanded(demand, existing_stock):
+    for stock in existing_stock.values():
+        demand -= stock
+    return demand
+
+
+def calc_surplus_retirement_proportion(surplus, vintage_specific_existing_stock):
+    if vintage_specific_existing_stock <= 0:
+        retirement_proportion = 0
+    else:
+        retirement_proportion = max(0, min(surplus/vintage_specific_existing_stock, 1))
+    return retirement_proportion
+
+
+def retire_surplus_stock(model, node, year, new_stock_demanded, existing_stock):
+    surplus = -1 * new_stock_demanded
+
+    # Base Stock Retirement
+    total_base_stock = 0
+    for tech in existing_stock:
+        tech_base_stock = model.get_param('base_stock_remaining', node, year, tech)
+        total_base_stock += tech_base_stock
+
+    if total_base_stock > 0:
+        retirement_proportion = calc_surplus_retirement_proportion(surplus, total_base_stock)
+        for tech in existing_stock:
+            tech_base_stock = model.get_param('base_stock_remaining', node, year, tech)
+            amount_tech_to_retire = tech_base_stock * retirement_proportion
+            existing_stock[tech] -= amount_tech_to_retire
+            surplus -= amount_tech_to_retire
+            new_stock_demanded += amount_tech_to_retire
+
+    # New Stock Retirement
+    possible_purchase_years = [y for y in model.years if (int(y) > model.base_year) &
+                                                         (int(y) < int(year))]
+    for purchase_year in possible_purchase_years:
+        total_new_stock_remaining_pre_surplus = 0
+        if surplus > 0:
+            for tech in existing_stock:
+                tech_remaining_stock_pre_surplus = model.get_param('new_stock_remaining_pre_surplus', node, year, tech)[purchase_year]
+                total_new_stock_remaining_pre_surplus += tech_remaining_stock_pre_surplus
+
+        retirement_proportion = calc_surplus_retirement_proportion(surplus, total_new_stock_remaining_pre_surplus)
+        for tech in existing_stock:
+            tech_data = model.graph.nodes[node][year]['technologies'][tech]
+            tech_remaining_stock_pre_surplus = model.get_param('new_stock_remaining_pre_surplus', node, year, tech)[purchase_year]
+            amount_tech_to_retire = tech_remaining_stock_pre_surplus * retirement_proportion
+            # Remove from existing stock
+            existing_stock[tech] -= amount_tech_to_retire
+            # Remove from surplus & new stock demanded
+            surplus -= amount_tech_to_retire
+            new_stock_demanded += amount_tech_to_retire
+            # note new stock remaining (post surplus) in the model
+            tech_data['new_stock_remaining']['year_value'][purchase_year] -= amount_tech_to_retire
+
+
+def calculate_new_market_shares(model, node, year):
+    new_market_shares_per_tech = {}
+    for tech in model.get_param('technologies', node, year):
+        new_market_shares_per_tech[tech] = {}
+        ms, ms_source = model.get_param('Market share', node, year, tech, return_source=True)
+
+        if ms_source == 'model':  # 'model' -> exogenous
+            new_market_share = ms
+        else:
+            new_market_share = 0
+            first_year_available = model.get_param('Available', node, str(model.base_year), tech)
+            first_year_unavailable = model.get_param('Unavailable', node, str(model.base_year), tech)
+            if first_year_available <= int(year) < first_year_unavailable:
+                v = model.get_param('Heterogeneity', node, year)
+                tech_lcc = model.get_param('Life Cycle Cost', node, year, tech)
+                total_weight = model.get_param('total_lcc_v', node, year)
+
+                # TODO: Instead of calculating this in 2 places, set this value in
+                #  lcc_calculation.py. Or here. Not both.
+                if tech_lcc < 0.01:
+                    # When lcc < 0.01, we will approximate it's weight using a TREND line
+                    w1 = 0.1 ** (-1 * v)
+                    w2 = 0.01 ** (-1 * v)
+                    slope = (w2 - w1) / (0.01 - 0.1)
+                    weight = slope * tech_lcc + (w1 - slope * 0.1)
+                else:
+                    weight = tech_lcc ** (-1 * v)
+
+                new_market_share = weight / total_weight
+        model.graph.nodes[node][year]['technologies'][tech]['base_stock'] = utils.create_value_dict(0, param_source='initialization')
+        model.graph.nodes[node][year]['technologies'][tech]['new_stock'] = utils.create_value_dict(0, param_source='initialization')
+        new_market_shares_per_tech[tech] = new_market_share
+
+    return new_market_shares_per_tech
+
+
+def calculate_total_market_shares(model, node, year, assessed_demand, existing_stock, new_market_shares):
+    total_market_shares_by_tech = {}
+    for tech in model.get_param('technologies', node, year):
+        try:
+            stock = existing_stock[tech]
+        except KeyError:
+            stock = 0
+
+        tech_total_stock = existing_stock + new_market_shares[tech] * assessed_demand
+
+        if assessed_demand == 0:
+            if model.show_run_warnings:
+                warnings.warn("Assessed Demand is 0 for {}[{}]".format(node, tech))
+            total_market_share = 0
+        else:
+            total_market_share = tech_total_stock / assessed_demand
+
+        total_market_shares_by_tech[tech] = total_market_share
+
+    return total_market_shares_by_tech
+
+
+def record_provided_quantities(model, year, node, tech, requested_services, assessed_demand,
+                               market_share=1):
+    if isinstance(requested_services, dict):
+        requested_services = [requested_services]
+
+    for service_data in requested_services:
+        service_req_ratio = service_data['year_value']
+        quant_requested = market_share * service_req_ratio * assessed_demand
+        year_node = model.graph.nodes[service_data['branch']][year]
+        if 'provided_quantities' not in year_node.keys():
+            year_node['provided_quantities'] = utils.create_value_dict(ProvidedQuantity(),
+                                                                       param_source='initialization')
+        year_node['provided_quantities']['year_value'].provide_quantity(amount=quant_requested,
+                                                                        requesting_node=node,
+                                                                        requesting_technology=tech)
+
+
+def record_allocation_results(model, node, year, adjusted_new_ms, total_market_shares, new_stock_demanded, assessed_demand):
+    for tech in model.get_param('technologies', node, year):
+        nms = adjusted_new_ms[tech]
+        model.graph.nodes[node][year]['technologies'][tech]['new_market_share'] = utils.create_value_dict(nms, param_source='calculation')
+
+        if int(year) == model.base_year:
+            model.graph.nodes[node][year]['technologies'][tech]['base_stock'] = utils.create_value_dict(new_stock_demanded * nms, param_source='calculation')
+        else:
+            model.graph.nodes[node][year]['technologies'][tech]['new_stock'] = utils.create_value_dict(new_stock_demanded * nms, param_source='calculation')
+
+        tms = total_market_shares[tech]
+        model.graph.nodes[node][year]['technologies'][tech]['total_market_share'] = utils.create_value_dict(tms, param_source='calculation')
+
+        tech_data = model.get_param('technologies', node, year)[tech]
+        if 'Service requested' in tech_data.keys():
+            record_provided_quantities(services_being_requested, assessed_demand,
+                                       technology=tech, technology_market_share=tms)
+        elif 'Service requested' in model.graph.nodes[node][year].keys():
+            services_being_requested = [v for k, v in model.graph.nodes[node][year]['Service requested'].items()]
+            record_provided_quantities(services_being_requested, assessed_demand)
+
+
+def tech_compete_allocation(model, sub_graph, node, year):
+    # Demand Assessment -- find amount demanded of the node by requesting nodes/techs
+    assessed_demand = model.get_param('provided_quantities', node, year).get_total_quantity()
+
+    # Existing Tech Specific Stocks -- find existing stock remaining after vintage-based retirement
+    existing_stock_per_tech = get_existing_stock_per_tech(model, sub_graph, node, year)
+
+    # Retrofits -- TODO
+
+    # Capital Stock Availability -- Find how much new stock must be adopted to meet demand
+    new_stock_demanded = calc_new_stock_demanded(assessed_demand, existing_stock_per_tech)
+
+    # Surplus Retirement
+    if new_stock_demanded < 0:
+        retire_surplus_stock(model, node, year, new_stock_demanded, existing_stock_per_tech)
+
+    # New Tech Competition
+    new_market_shares_per_tech = calculate_new_market_shares(model, node, year)
+
+    # Min/Max Market Share Limits
+    adjusted_new_ms = apply_min_max_limits(model, node, year, new_market_shares_per_tech)
+
+    # Calculate Total Market Shares
+    total_market_shares = calculate_total_market_shares(model, node, year, assessed_demand,
+                                                        existing_stock_per_tech, adjusted_new_ms)
+
+    # Record Values in Model
+    record_allocation_results(model, node, year, adjusted_new_ms, total_market_shares,
+                              new_stock_demanded, assessed_demand)
+
+def node_tech_compete_allocation(model, sub_graph, node, year):
+    pass
+
+
+def general_allocation():
+    pass
+
+
+#############################
+# Min/Max Market Share Limits
+#############################
 def get_min_max_limits(model, node, year):
     """
     Find the minimum & maximum market share limits in a given year for all technologies at a
