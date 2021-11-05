ae7dc0cc91887e2954d99cb19be3541d491e7362
Jillian Anderson <12687428+jillianderson8@users.noreply.github.com>
2021-11-05 15:18:03 -0700

    Updating basic code to work in v0.1.0
    Former-commit-id: 93417a67e437c11de09421b242c46a2b50f33c01

--- tutorials/pyCIMS_basic.py
+++ tutorials/pyCIMS_basic.py
@@ -2,15 +2,18 @@
 import pyCIMS
 from pprint import pprint
 
-file = 'pycims_prototype/model_descriptions/pyCIMS_model_description_AB_value.xlsb'
+file = 'pycims_prototype/model_descriptions/pyCIMS_model_description_ALL_value.xlsb'
 
-my_validator = pyCIMS.ModelValidator(file)
+my_validator = pyCIMS.ModelValidator(infile=file,
+                                     sheet_map={'model': 'Lists',
+                                                'default_tech': 'Technology_Node templates'},
+                                     node_col='Node')
 my_validator.validate(raise_warnings=False)
 pprint(my_validator.warnings)
 
 # Create a model description reader
 my_reader = pyCIMS.ModelReader(infile=file,
-                               sheet_map={'model': 'Model',
+                               sheet_map={'model': 'Lists',
                                           'incompatible': 'Incompatible',
                                           'default_tech': 'Technology_Node templates'},
                                node_col='Node')
@@ -22,4 +25,4 @@ my_model = pyCIMS.Model(my_reader)
 my_model.run(show_warnings=False, max_iterations=5)
 
 # Log the result
-pyCIMS.log_model(model=my_model, output_file="Alberta_AB_values_log.csv")
+pyCIMS.log_model(model=my_model, output_file="ALL_values_log.csv")
