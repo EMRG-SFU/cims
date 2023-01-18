abe4ee5701a84e6142e77c4fa13bc0e0097cb551
Jillian Anderson <12687428+jillianderson8@users.noreply.github.com>
2023-01-17 16:46:09 -0800

    documenting calc_stock_demanded
    Former-commit-id: 208b8b67acff60edc6f31c67a23aab327d418fa5

--- pyCIMS/stock_allocation/macro_economics.py
+++ pyCIMS/stock_allocation/macro_economics.py
@@ -1,3 +1,8 @@
+"""
+Module containing the functions required for performing Macro Economics calculations.
+"""
+
+
 def calc_total_stock_demanded(model, node, year):
     stock_demanded = calc_stock_demanded(model, node, year)
     stock_exported_region_1 = 0
@@ -9,15 +14,40 @@ def calc_total_stock_demanded(model, node, year):
 
 
 def calc_stock_demanded(model, node, year):
-    sum_service_stock_requested = model.get_param('provided_quantities', node, year).get_total_quantity()
-    price_t = model.get_param('price', node, year)
-    base_price = model.get_param('price', node, str(model.base_year))
-    domestic_elasticity = model.get_param('domestic elasticity', node, year)
+    """
+    Calculate the Stock Demanded for a node in a particular year. The result is used in the
+    calc_total_stock_demanded() function.
 
-    try:
-        stock_demanded = sum_service_stock_requested * (price_t/base_price)**domestic_elasticity
-    except:
-        stock_demanded = 0
-        if year != '2000':
-            jillian = 1
+    Stock Demanded is calculated by applying a macro-multiplier to the stock demanded of node by
+    other nodes in the model. The macro-multiplier is calculated using the node's relative price
+    and elasticity terms.
+
+    Parameters
+    ----------
+    model : pyCIMS.Model
+        The model containing the data required for calculation.
+    node : str
+        The node whose Stock Demanded is being calculated.
+    year : str
+        The year for which Stock Demanded is being calculated.
+
+    Returns
+    -------
+    float :
+        Returns the Stock Demanded for a node in a particular year.
+
+        Stock Demanded is calculated by applying a macro-multiplier to the stock demanded of node by
+        other nodes in the model. The macro-multiplier is calculated using the node's relative price
+        and elasticity terms.
+    """
+    sum_service_stock_requested = model.get_param('provided_quantities', node,
+                                                  year).get_total_quantity()
+
+    price_t = max([model.get_param('price', node, year), 0.01])
+    price_2000 = max([model.get_param('price', node, str(model.base_year)), 0.01])
+    domestic_elasticity = model.get_param('domestic elasticity', node, year)
+    macro_multiplier = (price_t / price_2000) ** domestic_elasticity
+
+    stock_demanded = sum_service_stock_requested * macro_multiplier
+
     return stock_demanded
