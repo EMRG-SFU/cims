2ffa43a9af9c83478e1b0bd21ffde312b5729903
Jillian Anderson <12687428+jillianderson8@users.noreply.github.com>
2023-03-06 22:46:01 -0800

    Updating example script
    Former-commit-id: 614b538f53ee05ce0be1bb82e0e3e66f245c0dbe

--- tutorials/pyCIMS_multiple_models.py
+++ tutorials/pyCIMS_multiple_models.py
@@ -1,8 +1,8 @@
 # Multiple Models
 import pyCIMS
 
-reference_model = 'pycims_prototype/models/multiple_models/pyCIMS_model_reference.xlsb'
-scenario_model = 'pycims_prototype/models/multiple_models/pyCIMS_model_scenario1.xlsb'
+reference_model_file = 'pycims_prototype/models/multiple_models/pyCIMS_model_reference.xlsb'
+scenario_model_file = 'pycims_prototype/models/multiple_models/pyCIMS_model_scenario1.xlsb'
 
 # ***************************************************
 # Design Option 1
@@ -10,7 +10,7 @@ scenario_model = 'pycims_prototype/models/multiple_models/pyCIMS_model_scenario1
 # Reference
 # =========
 # Create reference model reader
-reference_reader = pyCIMS.ModelReader(infile=reference_model,
+reference_reader = pyCIMS.ModelReader(infile=reference_model_file,
                                       sheet_map={'model': 'Lists',
                                                  'incompatible': 'Incompatible',
                                                  'default_param': 'Default values'},
@@ -21,10 +21,12 @@ reference_model = pyCIMS.Model(reference_reader)
 # Scenario 1
 # ==========
 # create scenario reader
-scenario_1_reader = pyCIMS.ModelReader(infile=scenario_model,
+scenario_1_reader = pyCIMS.ModelReader(infile=scenario_model_file,
                                        sheet_map={'model': 'Lists'},
                                        node_col='Node')
 
 
 # Update reference model with scenario reader
 scenario_1_model = reference_model.update(scenario_1_reader)
+
+scenario_1_model.run(show_warnings=False, max_iterations=5)
