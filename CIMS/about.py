9e49c6f75d6b1b8be270eab78cbe76a28b7596a8
Jillian Anderson <12687428+jillianderson8@users.noreply.github.com>
2024-03-12 14:17:48 -0700

    adding model download code

---
+++ CIMS/about.py
@@ -0,0 +1,5 @@
+# fmt: off
+__title__ = "CIMS"
+__version__ = "v0.1.3"
+__download_url__ = "https://api.github.com/repos/EMRG-SFU/cims-models/releases"
+__compatibility__ = "https://raw.githubusercontent.com/EMRG-SFU/cims-models/main/compatibility.json"
