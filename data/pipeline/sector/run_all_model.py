5c2d240b23e5d959bdc86c81af673243885be1fc
RenateBluschke <Renate.Bluschke@cer-rec.gc.ca>
2026-06-03 16:12:06 -0600

    added electricity to run_all_model script

--- data/pipeline/sector/run_all_model.py
+++ data/pipeline/sector/run_all_model.py
@@ -28,6 +28,7 @@ SCRIPTS = [
     SECTOR_DIR / "coal mining/model_inputs.py",
     SECTOR_DIR / "light industrial/model_inputs.py",
     SECTOR_DIR / "iron and steel/model_inputs.py",
+    SECTOR_DIR / "electricity/model_inputs.py",
 ]
 
 STOP_ON_ERROR = "--stop-on-error" in sys.argv[1:]
