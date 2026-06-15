26461d75cc519ede0f8a0ec54142a4ef8715c9ed
Gabi Diner <gabi.diner@cer-rec.gc.ca>
2026-06-15 13:49:56 -0700

    Adding model inputs calls

--- data/pipeline/sector/run_all_model.py
+++ data/pipeline/sector/run_all_model.py
@@ -20,6 +20,8 @@ SCRIPTS = [
     SECTOR_DIR / "chemical products/model_inputs.py",
     SECTOR_DIR / "commercial/model_inputs.py",
     SECTOR_DIR / "construction/model_inputs.py",
+    SECTOR_DIR / "electricity/model_inputs.py",
+    SECTOR_DIR / "exogenous demand/model_inputs.py",
     SECTOR_DIR / "forestry/model_inputs.py",
     SECTOR_DIR / "waste/model_inputs.py",
     SECTOR_DIR / "industrial minerals/model_inputs.py",
